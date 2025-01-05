86784df5aec8c7a87757f2a10f9d38092c610964
jackdar <jackdarlington2001@gmail.com>
2025-01-06 04:03:31 +1300

    feat: update chirp index on create new chirp, display chirps in list

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -3,10 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
     protected $fillable = [
         'message',
     ];
+
+    /**
+     * Get the user associated with a Chirp
+     *
+     * @return BelongsTo<User>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
